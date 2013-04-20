7e7fc593a740fe88a3c1758502594f5b80af500f
Yann Sionneau <yann.sionneau@gmail.com>
2013-04-20 16:27:06 +0200

    Add recursive-include protections in a few headers

--- sys/arch/milkymist/include/mutex.h
+++ sys/arch/milkymist/include/mutex.h
@@ -1,3 +1,7 @@
 /*	$NetBSD: mutex.h,v 1.2 2007/02/09 21:55:05 ad Exp $	*/
 
+#ifndef _MACHINE_MUTEX_H_
+#define _MACHINE_MUTEX_H_
 #include <lm32/mutex.h>
+
+#endif
--- sys/arch/milkymist/include/pmap.h
+++ sys/arch/milkymist/include/pmap.h
@@ -1,3 +1,8 @@
 /*	$NetBSD: pmap.h,v 1.45 2007/10/17 19:53:26 garbled Exp $	*/
 
+#ifndef _MACHINE_PMAP_H_
+#define _MACHINE_PMAP_H_
+
 #include <lm32/pmap.h>
+
+#endif
