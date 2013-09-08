1a19fba073a39abe27526b347ae64b351617b55c
Yann Sionneau <fallen@edgebsd.org>
2013-09-08 21:49:31 +0200

    Very basic procfs for lm32

---
+++ sys/arch/lm32/lm32/procfs_machdep.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2013 Yann Sionneau <yann.sionneau@gmail.com>
+ */
+
+#include <sys/cdefs.h>
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/mount.h>
+#include <sys/vnode.h>
+#include <miscfs/procfs/procfs.h>
+
+
+/*
+ * Linux-style /proc/cpuinfo.
+ * Only used when procfs is mounted with -o linux.
+ */
+int
+procfs_getcpuinfstr(char *buf, int *len)
+{
+	*len = 0;
+
+	return 0;
+}
