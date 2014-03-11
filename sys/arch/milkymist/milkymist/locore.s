f1ce747b4c98ec27e8bb418e4413a54e4c368145
Yann Sionneau <fallen@edgebsd.org>
2014-03-11 22:38:23 +0100

    _do_real_tlb_miss_handling and __isr were called with IRQ enabled
    If the TLB miss happens when IRQ are enabled, then
    PSW.EIE is set, doing eret with this PSW will then enable back IRQ
    (PSW.IE = PSW.EIE). We don't want to call the tls miss handler nor the irq
    handler with irq enabled.
    
    Therefore instead of reading PSW to modify it, we then start with a zero'ed register
    and only add the bits we want to enable the TLB when calling _do_real_tlb_miss_handling
    or __isr, and we end up writting this register to PSW.

--- sys/arch/milkymist/milkymist/locore.s
+++ sys/arch/milkymist/milkymist/locore.s
@@ -312,12 +312,8 @@ _ENTRY(_real_interrupt_handler)
   sub r2, r2, r3
   mvhi r3, 0x4000
   add r2, r2, r3
-  rcsr r3, PSW
+  xor r3, r3, r3
   ori r3, r3, 0x90 /* PSW_EDTLBE | PSW_EITLBE */
-  xor r4, r4, r4
-  ori r4, r4, 0x400 /* r4 = PSW_EUSR */
-  not r4, r4        /* r4 = ~(r4)    */
-  and r3, r3, r4    /* r3 &= ~PSW_EUSR */
   wcsr PSW, r3
   /* we then use eret as a trick to call __isr
   * with TLB ON and interrupts off */
@@ -506,12 +502,8 @@ out_of_ram_window:
   sub ra, ra, r4
   mvhi r4, 0x4000
   add ra, ra, r4
-  rcsr r4, PSW
+  xor r4, r4, r4
   ori r4, r4, 0x90 /* PSW_EDTLBE | PSW_EITLBE */
-  xor r5, r5, r5
-  ori r5, r5, 0x400 /* r4 = PSW_EUSR */
-  not r5, r5        /* r4 = ~(r4)    */
-  and r4, r4, r5    /* r3 &= ~PSW_EUSR */
   wcsr PSW, r4
   /* we then use eret as a trick to call _do_real_tlb_miss_handling
   * with TLB ON */
