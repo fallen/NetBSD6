8d239e03cd1736fc2c51bcb9718fe899c130e4d6
Yann Sionneau <fallen@edgebsd.org>
2014-07-03 10:04:45 +0200

    Fix assembly file indentation

--- sys/arch/milkymist/milkymist/locore.s
+++ sys/arch/milkymist/milkymist/locore.s
@@ -29,17 +29,17 @@
 #include "assym.h"
 
 /* Exception handlers - Must be 32 bytes long. */
-.section    .text, "ax", @progbits
-.global _start
-.type _start,@function
-.global start
-.type start,@function
+.section	.text, "ax", @progbits
+.global		_start
+.type		_start,@function
+.global		start
+.type		start,@function
 
 start:
 _bootstrap:
 	xor	r0, r0, r0
-  wcsr  PSW, r0
-/*mvhi	r1, hi(_reset_handler - _bootstrap)
+	wcsr	PSW, r0
+/*	mvhi	r1, hi(_reset_handler - _bootstrap)
 	ori	r1, r1, lo(_reset_handler) */
 	mvhi	r1, 0x4000
 	ori	r1, r1, lo(_start)
@@ -191,80 +191,80 @@ macaddress:
 _ENTRY(_fake_itlb_miss_handler)
 	mvhi	r0, 0x4000
 	ori	r0, r0, lo(_memory_store_area)
-  lw  r0, (r0+0)
+	lw 	r0, (r0+0)
 	sw	(r0+0), r1
 	sw	(r0+4), r2
-  lw  r1, (r0+-4) /* r1 = memory_store_area.first_jump_to_vaddr_done */
+	lw	r1, (r0+-4) /* r1 = memory_store_area.first_jump_to_vaddr_done */
 	xor	r0, r0, r0 /* restore r0 to 0 */
-  be  r1, r0, 1f
-
-  rcsr r1, TLBPADDR
-
-  mvhi r2, 0xffff
-  ori r2, r2, 0xf000
-  and r1, r1, r2 /* r1 &= ~(PG_MASK) */  
-  mvhi r2, 0xc000
-  sub r1, r1, r2
-  mvhi r2, 0x4000
-  add r1, r1, r2
- 
-  wcsr TLBPADDR, r1
+	be	r1, r0, 1f
+	
+	rcsr	r1, TLBPADDR
+	
+	mvhi	r2, 0xffff
+	ori	r2, r2, 0xf000
+	and	r1, r1, r2 /* r1 &= ~(PG_MASK) */  
+	mvhi	r2, 0xc000
+	sub	r1, r1, r2
+	mvhi	r2, 0x4000
+	add	r1, r1, r2
+	
+	wcsr	TLBPADDR, r1
 
 	mvhi	r0, 0x4000
 	ori	r0, r0, lo(_memory_store_area)
-  lw  r0, (r0+0)
+	lw	r0, (r0+0)
 	lw	r1, (r0+0)
 	lw	r2, (r0+4)
 	xor	r0, r0, r0 /* restore r0 to 0 */
-  eret
+	eret
 
 1:
-  mvhi r1, 0x4000
-  sub ea, ea, r1
-  mvhi r1, 0xc000
-  add ea, ea, r1
+	mvhi	r1, 0x4000
+	sub	ea, ea, r1
+	mvhi	r1, 0xc000
+	add	ea, ea, r1
 	mvhi	r0, 0x4000
 	ori	r0, r0, lo(_memory_store_area)
-  ori r2, r2, 0x42
-  lw  r0, (r0+0)
+	ori	r2, r2, 0x42
+	lw	r0, (r0+0)
 	sw	(r0+-4), r2 /* memory_store_area.first_jump_to_vaddr_done = true */
 	lw	r1, (r0+0)
 	lw	r2, (r0+4)
 	xor	r0, r0, r0 /* restore r0 to 0 */
-  eret
+	eret
 
 _ENTRY(_fake_dtlb_miss_handler)
 	mvhi	r0, 0x4000
 	ori	r0, r0, lo(_memory_store_area)
-  lw  r0, (r0+0)
+	lw	r0, (r0+0)
 	sw	(r0+0), r1
 	sw	(r0+4), r2
 	xor	r0, r0, r0 /* restore r0 to 0 */
 
-  rcsr r1, TLBPADDR
-
-  mvhi r2, 0xffff
-  ori r2, r2, 0xf000
-  and r1, r1, r2 /* r1 &= ~(PG_MASK) */  
-  mvhi r2, 0xc000
-  sub r1, r1, r2
-  mvhi r2, 0x4000
-  add r1, r1, r2
-  ori r1, r1, 1 /* writting to DTLB */ 
-  wcsr TLBPADDR, r1
+	rcsr	r1, TLBPADDR
+	
+	mvhi	r2, 0xffff
+	ori	r2, r2, 0xf000
+	and	r1, r1, r2 /* r1 &= ~(PG_MASK) */  
+	mvhi	r2, 0xc000
+	sub	r1, r1, r2
+	mvhi	r2, 0x4000
+	add	r1, r1, r2
+	ori	r1, r1, 1 /* writting to DTLB */ 
+	wcsr	TLBPADDR, r1
 
 	mvhi	r0, 0x4000
 	ori	r0, r0, lo(_memory_store_area)
-  lw  r0, (r0+0)
+	lw	r0, (r0+0)
 	lw	r1, (r0+0)
 	lw	r2, (r0+4)
 	xor	r0, r0, r0 /* restore r0 to 0 */
-  eret
+	eret
 
 _ENTRY(_real_interrupt_handler)
 	mvhi	r0, 0x4000
 	ori	r0, r0, lo(_memory_store_area)
-  lw  r0, (r0+0)
+	lw	r0, (r0+0)
 	sw	(r0+0), r1
 	sw	(r0+4), r2
 	sw	(r0+8), r3
@@ -284,56 +284,56 @@ _ENTRY(_real_interrupt_handler)
 	sw	(r0+64), r17
 	sw	(r0+68), r18
 	sw	(r0+72), r19
-	sw  (r0+76), r20
-	sw  (r0+80), r21
-  sw  (r0+84), r22
-  sw  (r0+88), r23
-  sw  (r0+92), r24
-  sw  (r0+96), r25
-  sw  (r0+100), gp
-  sw  (r0+104), fp
-  sw  (r0+108), sp
-	sw  (r0+112), ea
-	sw  (r0+116), ba
-	sw  (r0+120), ra
-  rcsr r3, PSW
-  sw  (r0+124), r3
+	sw	(r0+76), r20
+	sw	(r0+80), r21
+	sw	(r0+84), r22
+	sw	(r0+88), r23
+	sw	(r0+92), r24
+	sw	(r0+96), r25
+	sw	(r0+100), gp
+	sw	(r0+104), fp
+	sw	(r0+108), sp
+	sw	(r0+112), ea
+	sw	(r0+116), ba
+	sw	(r0+120), ra
+	rcsr	r3, PSW
+	sw	(r0+124), r3
 	xor	r0, r0, r0 /* restore r0 value to 0 */
   /* now update memory_store_area in case of nested tlb miss */
-  mvhi r1, 0x4000
-  ori r1, r1, lo(_memory_store_area)
-  lw r2, (r1+0)
-  mv r5, r2
-  addi r2, r2, 128
-  sw (r1+0), r2
+	mvhi	r1, 0x4000
+	ori	r1, r1, lo(_memory_store_area)
+	lw	r2, (r1+0)
+	mv	r5, r2
+	addi	r2, r2, 128
+	sw	(r1+0), r2
 
-  rcsr r1, IP
+	rcsr	r1, IP
 	mvhi	ea, hi(__isr) /* function we want to call */
 	ori	ea, ea, lo(__isr)
-  mvhi r2, hi(1f)                          /* where we want to return back to */
-  ori r2, r2, lo(1f)
-  mvhi  r3, 0xc000
-  sub r2, r2, r3
-  mvhi r3, 0x4000
-  add r2, r2, r3
-  xor r3, r3, r3
-  ori r3, r3, 0x90 /* PSW_EDTLBE | PSW_EITLBE */
-  wcsr PSW, r3
-  mv r3, r5 /* arg3 is trapframe */
-  /* we then use eret as a trick to call __isr
-  * with TLB ON and interrupts off */
-  eret
+	mvhi	r2, hi(1f)                          /* where we want to return back to */
+	ori	r2, r2, lo(1f)
+	mvhi	r3, 0xc000
+	sub	r2, r2, r3
+	mvhi	r3, 0x4000
+	add	r2, r2, r3
+	xor	r3, r3, r3
+	ori	r3, r3, 0x90 /* PSW_EDTLBE | PSW_EITLBE */
+	wcsr	PSW, r3
+	mv	r3, r5 /* arg3 is trapframe */
+	/* we then use eret as a trick to call __isr
+	* with TLB ON and interrupts off */
+	eret
   
 
 1:
 	mvhi	r0, 0x4000
 	ori	r0, r0, lo(_memory_store_area)
-  lw r1, (r0+0)
-  addi r1, r1, -128
-  sw (r0+0), r1
-  addi r0, r1, 0 /* we cannot use 'mv' when r0 != 0 */
+	lw	r1, (r0+0)
+	addi	r1, r1, -128
+	sw	(r0+0), r1
+	addi	r0, r1, 0 /* we cannot use 'mv' when r0 != 0 */
 	lw	r1, (r0+124)
-  wcsr PSW, r1
+	wcsr	PSW, r1
 	lw	r1, (r0+0)
 	lw	r2, (r0+4)
 	lw	r3, (r0+8)
@@ -361,129 +361,129 @@ _ENTRY(_real_interrupt_handler)
 	lw	r25, (r0+96)
 	lw	gp, (r0+100)
 	lw	fp, (r0+104)
-  lw  sp, (r0+108)
+	lw	sp, (r0+108)
 	lw	ea, (r0+112)
 	lw	ba, (r0+116)
 	lw	ra, (r0+120)
 	xor	r0, r0, r0 /* restore r0 value to 0 */
-  eret
+	eret
 
 _ENTRY(_real_tlb_miss_handler)
 	mvhi	r0, 0x4000
 	ori	r0, r0, lo(_memory_store_area)
-  lw  r0, (r0+0)
-	sw	(r0+0), r1
-	sw	(r0+4), r2
-	sw	(r0+8), r3
-	sw	(r0+12), r4
-	sw	(r0+16), r5
-	sw	(r0+20), r6
-	sw	(r0+24), r7
-	sw	(r0+28), r8
-	sw	(r0+32), r9
-	sw	(r0+36), r10
-	sw	(r0+40), r11
-	sw	(r0+44), r12
-	sw	(r0+48), r13
-	sw	(r0+52), r14
-	sw	(r0+56), r15
-	sw	(r0+60), r16
-	sw	(r0+64), r17
-	sw	(r0+68), r18
-	sw	(r0+72), r19
-	sw  (r0+76), r20
-	sw  (r0+80), r21
-  sw  (r0+84), r22
-  sw  (r0+88), r23
-  sw  (r0+92), r24
-  sw  (r0+96), r25
-  sw  (r0+100), gp
-  sw  (r0+104), fp
-  sw  (r0+108), sp
-	sw  (r0+112), ea
-	sw  (r0+116), ba
-	sw  (r0+120), ra
-  rcsr r3, PSW
-  sw  (r0+124), r3
+	lw	r0, (r0+0)
+	sw	(r0+0),   r1
+	sw	(r0+4),   r2
+	sw	(r0+8),   r3
+	sw	(r0+12),  r4
+	sw	(r0+16),  r5
+	sw	(r0+20),  r6
+	sw	(r0+24),  r7
+	sw	(r0+28),  r8
+	sw	(r0+32),  r9
+	sw	(r0+36),  r10
+	sw	(r0+40),  r11
+	sw	(r0+44),  r12
+	sw	(r0+48),  r13
+	sw	(r0+52),  r14
+	sw	(r0+56),  r15
+	sw	(r0+60),  r16
+	sw	(r0+64),  r17
+	sw	(r0+68),  r18
+	sw	(r0+72),  r19
+	sw	(r0+76),  r20
+	sw	(r0+80),  r21
+	sw	(r0+84),  r22
+	sw	(r0+88),  r23
+	sw	(r0+92),  r24
+	sw	(r0+96),  r25
+	sw	(r0+100), gp
+	sw	(r0+104), fp
+	sw	(r0+108), sp
+	sw	(r0+112), ea
+	sw	(r0+116), ba
+	sw	(r0+120), ra
+	rcsr	r3, PSW
+	sw	(r0+124), r3
 	xor	r0, r0, r0 /* restore r0 value to 0 */
-  /* now update memory_store_area in case of nested tlb miss */
-  mvhi r1, 0x4000
-  ori r1, r1, lo(_memory_store_area)
-  lw r2, (r1+0)
-  addi r2, r2, 128
-  sw (r1+0), r2
-
-  rcsr r1, TLBVADDR
-  rcsr r2, TLBPADDR
-  andi r3, r3, PSW_EUSR /* r3 &= PSW_EUSR */
-  bne r3, r0, we_come_from_user_space
-  mvhi r3, 0xffff
-  ori r3, r3, 0xf07f /* r3 = ~(TLBVADDR_ASID_MASK); */
-  and r1, r1, r3
-  wcsr TLBVADDR, r1
-  mvhi r3, 0xc800
-  cmpgeu r4, r1, r3
-  bne r4, r0, out_of_ram_window
-  mvhi r3, 0xc000
-  cmpgu r4, r3, r1
-  bne r4, r0, out_of_ram_window
-  mvhi r3, 0xc000
-  sub r1, r1, r3
-  mvhi r3, 0x4000
-  add r1, r1, r3
-  wcsr TLBPADDR, r1
-  bi 1f /* let's return to what we were doing */
+	/* now update memory_store_area in case of nested tlb miss */
+	mvhi	r1, 0x4000
+	ori	r1, r1, lo(_memory_store_area)
+	lw	r2, (r1+0)
+	addi	r2, r2, 128
+	sw	(r1+0), r2
+	
+	rcsr	r1, TLBVADDR
+	rcsr	r2, TLBPADDR
+	andi	r3, r3, PSW_EUSR /* r3 &= PSW_EUSR */
+	bne	r3, r0, we_come_from_user_space
+	mvhi	r3, 0xffff
+	ori	r3, r3, 0xf07f /* r3 = ~(TLBVADDR_ASID_MASK); */
+	and	r1, r1, r3
+	wcsr	TLBVADDR, r1
+	mvhi	r3, 0xc800
+	cmpgeu	r4, r1, r3
+	bne	r4, r0, out_of_ram_window
+	mvhi	r3, 0xc000
+	cmpgu	r4, r3, r1
+	bne	r4, r0, out_of_ram_window
+	mvhi	r3, 0xc000
+	sub	r1, r1, r3
+	mvhi	r3, 0x4000
+	add	r1, r1, r3
+	wcsr	TLBPADDR, r1
+	bi	1f /* let's return to what we were doing */
 
 out_of_ram_window:
-  rcsr r1, TLBVADDR
-  mvhi r4, hi(_C_LABEL(cpu_info_store))
-  ori  r4, r4, lo(_C_LABEL(cpu_info_store))
-  lw   r4, (r4+CPU_INFO_CURPM)
-  lw   r4, (r4+PM_SEGTAB) /* r4 = curcpu()->ci_pm->pm_segtab; */
-  srui r5, r1, SEGSHIFT
-  sli  r5, r5, 2
-  add  r6, r4, r5
-
-  calli check_page_table
-  be  r2, r0, 1f
+	rcsr	r1, TLBVADDR
+	mvhi	r4, hi(_C_LABEL(cpu_info_store))
+	ori	r4, r4, lo(_C_LABEL(cpu_info_store))
+	lw	r4, (r4+CPU_INFO_CURPM)
+	lw	r4, (r4+PM_SEGTAB) /* r4 = curcpu()->ci_pm->pm_segtab; */
+	srui	r5, r1, SEGSHIFT
+	sli	r5, r5, 2
+	add	r6, r4, r5
+	
+	calli	check_page_table
+	be	r2, r0, 1f
 
 we_come_from_user_space:
-  mvhi r4, hi(_C_LABEL(cpu_info_store))
-  ori  r4, r4, lo(_C_LABEL(cpu_info_store))
-  lw   r4, (r4+CPU_INFO_USER_SEGTAB) /* r4 = curcpu()->ci_pmap_user_segtab; */
-  srui r5, r1, SEGSHIFT
-  sli  r5, r5, 2
-  add  r6, r4, r5
-
-  calli check_page_table
-  be  r2, r0, 1f
+	mvhi	r4, hi(_C_LABEL(cpu_info_store))
+	ori	r4, r4, lo(_C_LABEL(cpu_info_store))
+	lw	r4, (r4+CPU_INFO_USER_SEGTAB) /* r4 = curcpu()->ci_pmap_user_segtab; */
+	srui	r5, r1, SEGSHIFT
+	sli	r5, r5, 2
+	add	r6, r4, r5
+	
+	calli	check_page_table
+	be	r2, r0, 1f
 
 goto_trap:
-  mvhi	r2, 0x4000
-  ori	r2, r2, lo(_memory_store_area)
-  lw r2, (r2+0)
-  mvhi ea, hi(lm32_trap)
-  ori  ea, ea, lo(lm32_trap)
-  xor r3, r3, r3
-  ori r3, r3, 0x92 /* EDTLBE | EITLBE | EIE */
-  wcsr PSW, r3
-  mvhi r4, 0xc000
-  mvhi r3, hi(1f)
-  ori  r3, r3, lo(1f)
-  sub  r3, r3, r4
-  mvhi r4, 0x4000
-  add r3, r3, r4
-  eret
+	mvhi	r2, 0x4000
+	ori	r2, r2, lo(_memory_store_area)
+	lw	r2, (r2+0)
+	mvhi	ea, hi(lm32_trap)
+	ori	ea, ea, lo(lm32_trap)
+	xor	r3, r3, r3
+	ori	r3, r3, 0x92 /* EDTLBE | EITLBE | EIE */
+	wcsr	PSW, r3
+	mvhi	r4, 0xc000
+	mvhi	r3, hi(1f)
+	ori	r3, r3, lo(1f)
+	sub	r3, r3, r4
+	mvhi	r4, 0x4000
+	add	r3, r3, r4
+	eret
 
 1:
 	mvhi	r0, 0x4000
 	ori	r0, r0, lo(_memory_store_area)
-  lw r1, (r0+0)
-  addi r1, r1, -128
-  sw (r0+0), r1
-  addi r0, r1, 0 /* we cannot use 'mv' when r0 != 0 */
+	lw	r1, (r0+0)
+	addi	r1, r1, -128
+	sw	(r0+0), r1
+	addi	r0, r1, 0 /* we cannot use 'mv' when r0 != 0 */
 	lw	r1, (r0+124)
-  wcsr PSW, r1
+	wcsr	PSW, r1
 	lw	r1, (r0+0)
 	lw	r2, (r0+4)
 	lw	r3, (r0+8)
@@ -511,7 +511,7 @@ goto_trap:
 	lw	r25, (r0+96)
 	lw	gp, (r0+100)
 	lw	fp, (r0+104)
-  lw  sp, (r0+108)
+	lw	sp, (r0+108)
 	lw	ea, (r0+112)
 	lw	ba, (r0+116)
 	lw	ra, (r0+120)
@@ -519,119 +519,119 @@ goto_trap:
 	eret
 
 check_page_table:
-  lw   r7, (r6+0) /* ptp = pm_segtab[vaddr >> SEGSHIFT]; */
-  be   r7, r0, ptp_not_found
-
-  /* translate the ptp from virt to phy */
-  mvhi r10, 0xc000
-  sub  r7, r7, r10
-  mvhi r10, 0x4000
-  add  r7, r7, r10
-
-  mvhi r11, hi(L2_MASK)
-  ori  r11, r11, lo(L2_MASK)
-  and  r8, r1, r11
-  srui  r8, r8, PGSHIFT
-  sli   r8, r8, 2
-  add  r8, r8, r7
-  lw   r9, (r8+0) /* pte = ptp[(vaddr & L2_MASK) >> PGSHIFT] ; */
-
-  be  r9, r0, pte_not_found
+	lw	r7, (r6+0) /* ptp = pm_segtab[vaddr >> SEGSHIFT]; */
+	be	r7, r0, ptp_not_found
+	
+	/* translate the ptp from virt to phy */
+	mvhi	r10, 0xc000
+	sub	r7, r7, r10
+	mvhi	r10, 0x4000
+	add 	r7, r7, r10
+	
+	mvhi	r11, hi(L2_MASK)
+	ori	r11, r11, lo(L2_MASK)
+	and	r8, r1, r11
+	srui	r8, r8, PGSHIFT
+	sli	r8, r8, 2
+	add	r8, r8, r7
+	lw	r9, (r8+0) /* pte = ptp[(vaddr & L2_MASK) >> PGSHIFT] ; */
+	
+	be	r9, r0, pte_not_found
 
 /* TODO: check for PTE_xX,PTE_xR, PTE_xW before assigning the rights */
 /* for now all mappings are read-write-execute */
 
-  andi r2, r1, 1  /* if (vpfn & 1) { */
-  be   r2, r0, not_a_dtlb_miss
-  ori  r9, r9, 1 /* pte |= 1; meaning we refresh DTLB */
-  mvhi r2, 0xffff
-  ori  r2, r2, 0xfffd
-  and r9, r9, r2 /* pte &= ~(2); clearing Read-Only bit */
+	andi	r2, r1, 1  /* if (vpfn & 1) { */
+	be	r2, r0, not_a_dtlb_miss
+	ori	r9, r9, 1 /* pte |= 1; meaning we refresh DTLB */
+	mvhi	r2, 0xffff
+	ori	r2, r2, 0xfffd
+	and	r9, r9, r2 /* pte &= ~(2); clearing Read-Only bit */
 
 not_a_dtlb_miss: /* } */
-  wcsr TLBPADDR, r9
-  xor r2, r2, r2 /* r2 = EXIT_SUCCESS */
-  ret
+	wcsr	TLBPADDR, r9
+	xor	r2, r2, r2 /* r2 = EXIT_SUCCESS */
+	ret
 
 ptp_not_found:
 pte_not_found:
-  mvi r2, 1 /* r2 = EXIT_FAILURE */
-  ret
+	mvi	r2, 1 /* r2 = EXIT_FAILURE */
+	ret
 
 
 _crt0:
-  mvhi  r1, 0x4000
-  ori r1, r1, lo(_memory_store_area)
-  lw  r2, (r1+0)
-  addi  r2, r2, 4
-  sw (r1+0), r2
+	mvhi	r1, 0x4000
+	ori	r1, r1, lo(_memory_store_area)
+	lw	r2, (r1+0)
+	addi	r2, r2, 4
+	sw	(r1+0), r2
 	/* activate ITLB and DTLB */
 	mvi	r1, 0x48
-	wcsr 	PSW, r1
+	wcsr	PSW, r1
 
 	/* stack and global pointers 
-   * should be initialized
-   * by bootloader/BIOS
-   */
+	* should be initialized
+	* by bootloader/BIOS
+	*/
 	/* Setup stack and global pointer */
-	mvhi    sp, hi(_fstack)
-	ori     sp, sp, lo(_fstack)
-	mvhi    gp, hi(_gp)
-	ori     gp, gp, lo(_gp)
+	mvhi	sp, hi(_fstack)
+	ori	sp, sp, lo(_fstack)
+	mvhi	gp, hi(_gp)
+	ori	gp, gp, lo(_gp)
 
 	/* Clear BSS */
-	mvhi    r1, hi(_fbss)
-	ori     r1, r1, lo(_fbss)
-	mvhi    r3, hi(_ebss)
-	ori     r3, r3, lo(_ebss)
+	mvhi	r1, hi(_fbss)
+	ori	r1, r1, lo(_fbss)
+	mvhi	r3, hi(_ebss)
+	ori	r3, r3, lo(_ebss)
 .clearBSS:
-	be      r1, r3, .callMain
-	sw      (r1+0), r0
-	addi    r1, r1, 4
-	bi      .clearBSS
+	be	r1, r3, .callMain
+	sw	(r1+0), r0
+	addi	r1, r1, 4
+	bi	.clearBSS
 
 .callMain:	
-	mv      r1, r2
-	mvi     r2, 0
-	mvi     r3, 0
+	mv	r1, r2
+	mvi	r2, 0
+	mvi	r3, 0
 	mvhi	r4, hi(milkymist_startup)
 	ori	r4, r4, lo(milkymist_startup)
 	b	r4
 
 .save_all:
-	addi    sp, sp, -56
-	sw      (sp+4), r1
-	sw      (sp+8), r2
-	sw      (sp+12), r3
-	sw      (sp+16), r4
-	sw      (sp+20), r5
-	sw      (sp+24), r6
-	sw      (sp+28), r7
-	sw      (sp+32), r8
-	sw      (sp+36), r9
-	sw      (sp+40), r10
-	sw      (sp+48), ea
-	sw      (sp+52), ba
+	addi	sp, sp, -56
+	sw	(sp+4), r1
+	sw	(sp+8), r2
+	sw	(sp+12), r3
+	sw	(sp+16), r4
+	sw	(sp+20), r5
+	sw	(sp+24), r6
+	sw	(sp+28), r7
+	sw	(sp+32), r8
+	sw	(sp+36), r9
+	sw	(sp+40), r10
+	sw	(sp+48), ea
+	sw	(sp+52), ba
 	/* ra needs to be moved from initial stack location */
-	lw      r1, (sp+56)
-	sw      (sp+44), r1
+	lw	r1, (sp+56)
+	sw	(sp+44), r1
 	ret
 
 .restore_all_and_eret:
-	lw      r1, (sp+4)
-	lw      r2, (sp+8)
-	lw      r3, (sp+12)
-	lw      r4, (sp+16)
-	lw      r5, (sp+20)
-	lw      r6, (sp+24)
-	lw      r7, (sp+28)
-	lw      r8, (sp+32)
-	lw      r9, (sp+36)
-	lw      r10, (sp+40)
-	lw      ra, (sp+44)
-	lw      ea, (sp+48)
-	lw      ba, (sp+52)
-	addi    sp, sp, 56
+	lw	r1, (sp+4)
+	lw	r2, (sp+8)
+	lw	r3, (sp+12)
+	lw	r4, (sp+16)
+	lw	r5, (sp+20)
+	lw	r6, (sp+24)
+	lw	r7, (sp+28)
+	lw	r8, (sp+32)
+	lw	r9, (sp+36)
+	lw	r10, (sp+40)
+	lw	ra, (sp+44)
+	lw	ea, (sp+48)
+	lw	ba, (sp+52)
+	addi	sp, sp, 56
 	eret
 
 .section    .rodata, "a", @progbits
