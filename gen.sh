eb2c1ecc3203d1037092cb040b8978eb59ca9a64
Yann Sionneau <yann@sionneau.net>
2019-03-31 19:52:27 +0200

    some basic steps to try and build /rescue

--- gen.sh
+++ gen.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-HAVE_GCC=45 USE_COMPILERCRTSTUFF=no MKPIC=no MKATF=no MKGCC=no MKGCCCMDS=no MKBINUTILS=no MKGDB=no MKCRYPTO=no MKCOMPLEX=no MKDOC=no MKDYNAMICROOT=no MKHTML=no MKINET6=no MKINFO=no MKIPFILTER=no MKISCSI=no MKLDAP=no MKKMOD=no MKKERBEROS=no MKLIBSTDCXX=no MKLVM=no MKMAN=no MKLINT=no MKPOSTFIX=no MKPROFILE=no MKSHARE=no ./build.sh -m milkymist -u -U $* build
+NBMAKE=obj/tooldir.$(uname -s)-$(uname -r)-$(uname -m)/bin/nbmake-milkymist
+
+./build.sh -m milkymist -u -U tools
+./build.sh -m milkymist kernel=GENERIC
+HAVE_GCC=45 USE_COMPILERCRTSTUFF=no MKPIC=no MKATF=no MKGCC=no MKGCCCMDS=no MKBINUTILS=no MKGDB=no MKCOMPLEX=no MKDOC=no MKDYNAMICROOT=no MKHTML=no MKINET6=no MKINFO=no MKIPFILTER=no MKISCSI=no MKLDAP=no MKKMOD=no MKKERBEROS=no MKLIBSTDCXX=no MKLVM=no MKMAN=no MKLINT=no MKPOSTFIX=no MKPROFILE=no MKSHARE=no ./build.sh -m milkymist -u -U $* build
+for lib in $(ls lib)
+do
+	cd $lib
+	$NBMAKE
+	$NBMAKE install
+	cd -
+done
+
+cd rescue
+$NBMAKE
+cd -
